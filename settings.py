cc8968bc9ca3271a4650ae5cce4d4666917a3892
Sam Knowles <sam.knowles@gmail.com>
2011-01-09 12:05:55 +1300

    Polls Tutorial

--- settings.py
+++ settings.py
@@ -89,8 +89,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'polls',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
