84bcb4ded2de5f675a1c9b2f208d7795779cf803
Sam Knowles <sam.knowles@gmail.com>
2011-01-09 13:43:58 +1300

    Complete Tutorial Part 2: admin pages.

--- polls/models.py
+++ polls/models.py
@@ -11,6 +11,8 @@ class Poll(models.Model):
     def was_published_today(self):
         return self.pub_date.date() == datetime.date.today()
 
+    was_published_today.short_description = 'Published today?'
+
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
