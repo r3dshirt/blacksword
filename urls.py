a3e7ed3085b6ec80fc65e36c4d26298b6903da23
Sam Knowles <sam.knowles@gmail.com>
2011-01-14 12:48:14 +1300

    Added static files, css/images etc.

--- urls.py
+++ urls.py
@@ -5,5 +5,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^polls/', include('polls.urls')),
     (r'^admin/', include(admin.site.urls)),
+    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/sam/Projects/blacksword/blacksword/site_media/'}),
     (r'^', 'home.views.index'),
 )
