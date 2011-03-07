1c3888020cd72e625fbfe3e8bd336cde49a3491a
Sam Knowles <sam.knowles@gmail.com>
2011-03-08 08:22:24 +1300

    simple kin_event index view.

--- kin_events/views.py
+++ kin_events/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.template import Context, loader
+from django.http import HttpResponse
+from django.http import Http404
+
+from kin_events.models import Event
+
+def index(request):
+    events_list = Event.objects.all().order_by('-date')[:5]
+    t = loader.get_template('kin_events/index.html')
+    c = Context({'events_list': events_list,})
+    return HttpResponse(t.render(c))
